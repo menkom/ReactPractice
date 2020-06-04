2c2edb79f8b042a7974da789a618fcb9025040c1
mikhail.menko <menko.mihail@gmail.com>
2020-06-04 22:10:54 +0300

    task9.2

---
+++ lesson9/task9.2.js
@@ -0,0 +1,25 @@
+// Реализовать функцию, которая первым аргументом принимает строку, вторым аргументом массив строк.
+// Первый аргумент является зашифрованным именем. Функция должна вернуть строку из подходящих имен.
+// Если ни одно имя не подходит вернуть пустую строку
+//
+// const names = [ 'Anastasia', 'C Powel', 'Wilfrid Stevens', 'Peter Brien', 'J Steeve',
+// 'Arthur Clarke', 'Bernard Deltheil', 'R Steell', 'Peter Gone', 'Peter Reeves', 'Roland Scorsini',
+// 'Bernard Povit', 'Peter Reedgrave', 'Raymond Stevenson', 'E Kustur', 'P McDon', 'Paul Dive', 'F Flanaghan',
+// 'C Saborn', 'John Freeland', 'Jr Part', 'Pete Highman', 'Arthur Paternos', 'P Reed', 'W Mount',
+// 'Paulo Divino', 'Sophia Loren', 'W Mount', 'Peter Pan', 'Anna Stevens', 'Laurence Pantow', 'Peter Crush',
+// 'Ray Charles', 'William Saurin', 'Donald Drinkaw', 'F Fulgur', 'Ray Chandler' ];
+//
+// findMurder ('~~T~~r~pa~e~No', names);//Arthur Paternos
+// findMurder (' ~oLA~D~sc~Rs~Ni', names);//Roland Scorsini
+// findMurder (' ~~~~~~~~NE', names);//Peter Gone
+// findMurder (' w~MOu~T', names);//'W Mount,W Mount'
+//
+// findMurder (' ~~~~r', names);//'Wilfrid Stevens,Peter Brien,Peter Gone,Peter Reeves,Peter Reedgrave,Peter Pan,Peter Crush'
+
+function findMurder(search, names) {
+    let result = '';
+
+    // names.
+
+    return result;
+}
